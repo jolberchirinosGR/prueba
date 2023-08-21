ba9dde57c87699377ef631989eaf776b0312e187
TheJC18 <JolberChirinos@gmail.com>
2023-08-21 08:07:20 +0100

    fixed route home and welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,1 @@
-@extends('adminlte::page')
-
-@section('content_header')
-    <h1>Welcome</h1>
-@stop
-
-@section('content')
-    <p>Welcome to this beautiful admin panel is Welcome.</p>
-@stop
-
+@extends('adminlte::auth.login')
